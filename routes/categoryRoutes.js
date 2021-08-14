43451842dcdc70696cfc29d91a2493b1f26222ed
HrDelwar <hrdelwar75@gmail.com>
2021-08-15 00:38:06 +0600

    create-seperate-controllers

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,81 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
+import {
+  getAllCategory,
+  getSingleCategoryById,
+  postCategory,
+  updateCategoryById,
+} from "../controllers/categoryController";
 import models from "../models";
 
 const router = express.Router();
 
 // Get all Category route
-router.get("/", async (req, res) => {
-  try {
-    const categories = await models.Category.find({});
-    if (categories.length <= 0) {
-      res.status(404).json({
-        name: "NotFound",
-        categories,
-        message: "Total document: " + categories.length,
-      });
-    } else {
-      res.status(200).json({
-        name: "All Categories",
-        categories,
-        message: "Total document: " + categories.length,
-      });
-    }
-  } catch (err) {
-    res.status(404).json({
-      name: "NotFound",
-      categories,
-      message: "Total document: " + categories.length,
-    });
-  }
-});
+router.get("/", getAllCategory);
 
 // get single category
 
-router.get("/:id", async (req, res) => {
-  await models.Category.find({ _id: req.params.id }, (err, docs) => {
-    if (err) {
-      res.status(404).json({
-        name: "NotFound",
-        message: "document not found by id: " + req.params.id,
-      });
-    } else {
-      res.status(200).json({
-        name: "Ok",
-        docs,
-        message: "Document found",
-      });
-    }
-  });
-});
+router.get("/:id", getSingleCategoryById);
 
 // Add new category
-router.post("/", async (req, res) => {
-  const newCategory = new models.Category(req.body);
-  await newCategory.save((err) => {
-    if (err) {
-      res.status(500).json({ error: "server side error" });
-    } else {
-      res.status(200).json({ message: "category added successfully" });
-    }
-  });
-});
+router.post("/", postCategory);
 
 // update category
 
-router.put("/:id", async (req, res) => {
-  const { name, shortDesc } = req.body;
-  await models.Category.updateOne(
-    { _id: req.params.id },
-    { $set: { name: name, shortDesc: shortDesc } },
-    (err) => {
-      if (err) {
-        res.status(500).json({ error: "server side error" });
-      } else {
-        res.status(200).json({ message: "category updated successfully" });
-      }
-    }
-  );
-});
+router.put("/:id", updateCategoryById);
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,49 +1,15 @@
-const bcrypt = require("bcrypt");
-const express = require("express");
+import express from 'express';
 
 const router = express.Router();
-import models from "../models";
+import { handleUserLogin, handleUserSignup } from "../controllers/userController";
+
 
 // signup route
 
-router.post("/signup", async (req, res) => {
-  const body = req.body;
+router.post("/signup", handleUserSignup);
 
-  if (!(body.email && body.password)) {
-    return res.status(400).send({ error: "Data not formated properly" });
-  }
-
-  // createing a new mongoose doc for user data
-  const user = new models.User(body);
-  try {
-    // generate salt to hash password
-    const salt = await bcrypt.genSalt(10);
-    // now we set user password to hashed password
-    user.password = await bcrypt.hash(user.password, salt);
-    const savedUser = await user.save();
-    res.status(201).send(savedUser);
-  } catch (err) {
-    res.send(501);
-    console.log(err);
-  }
-});
 
 // login route
-
-router.post("/login", async (req, res) => {
-  const body = req.body;
-  const user = await models.User.findOne({ email: body.email });
-  if (user) {
-    // check user password with hashed password stored in the db
-    const validPassword = await bcrypt.compare(body.password, user.password);
-    if (validPassword) {
-      res.status(200).json({ message: "valid password" });
-    } else {
-      res.status(400).json({ error: "Invalid password or email" });
-    }
-  } else {
-    res.status(401).json({ error: "User does not exist" });
-  }
-});
+router.post("/login", handleUserLogin);
 
 export default router;
