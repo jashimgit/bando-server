7da4e49c357ffd5416b98da7d631a80cfd10780c
HrDelwar <hrdelwar75@gmail.com>
2021-08-18 19:42:05 +0600

    modify auth access token

--- middlewares/verifyAuthToken.js
+++ middlewares/verifyAuthToken.js
@@ -2,34 +2,13 @@ import jwt from "jsonwebtoken";
 
 const verifyAuthToken = (req, res, next) => {
   const token = req.headers.token;
-  const {authorization} = req.headers;
-
+  
   if (!token)
     return res.status(401).send({
       name: "Access Denied",
       message: "access denied",
       success: false,
     });
-
-  // if(!authorization){
-  //   return res.status(401).send({
-  //     name: "Access Denied",
-  //     message: "access denied",
-  //     success: false,
-  //   });
-  // }
-
-  // try {
-  //   const token = authorization.split(' ')[1]
-  //   const user = jwt.verify(token, process.env.AUTH_SECRET);
-  //   req.user = user;
-  //   next();
-  // } catch (err) {
-  //   return res
-  //     .status(401)
-  //     .send({ name: "Access Denied", message: err.message, success: false });
-  // }
-
 
 
   try {
