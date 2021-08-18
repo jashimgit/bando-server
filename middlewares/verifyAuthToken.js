0fcbeeaabb0ee17cc6bc3442f6df3a565119dd96
jashimgit <jashim.ncn@gmail.com>
2021-08-18 12:52:33 +0600

    verifyAuthToken modified

--- middlewares/verifyAuthToken.js
+++ middlewares/verifyAuthToken.js
@@ -1,14 +1,37 @@
 import jwt from "jsonwebtoken";
 
 const verifyAuthToken = (req, res, next) => {
-  const token = req.headers.token;
-  if (!token)
+  // const token = req.headers.token;
+  const {authorization} = req.headers;
+
+  // if (!token)
+  //   return res.status(401).send({
+  //     name: "Access Denied",
+  //     message: "access denied",
+  //     success: false,
+  //   });
+
+  if(!authorization){
     return res.status(401).send({
       name: "Access Denied",
       message: "access denied",
       success: false,
     });
+  }
 
+  try {
+    const token = authorization.split(' ')[1]
+    const user = jwt.verify(token, process.env.AUTH_SECRET);
+    req.user = user;
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .send({ name: "Access Denied", message: err.message, success: false });
+  }
+
+
+/* 
   try {
     const user = jwt.verify(token, process.env.AUTH_SECRET);
     req.user = user;
@@ -18,6 +41,7 @@ const verifyAuthToken = (req, res, next) => {
       .status(401)
       .send({ name: "Access Denied", message: err.message, success: false });
   }
+  */
 };
 
 export default verifyAuthToken;
