2e1df2d02c1456c6a5bf03a07260b901cef28612
jashimgit <jashim.ncn@gmail.com>
2021-08-29 18:36:52 +0600

    get all sub category done

--- controllers/subCategoryController.js
+++ controllers/subCategoryController.js
@@ -8,8 +8,21 @@ import models from "../models";
 const { SubCategory } = models;
 const { Category } = models;
 
-export const getAllSubCategory = async () => {
-  const subCategory = await SubCategory.find({});
+export const getAllSubCategory = async (req, res) => {
+  await SubCategory.find({})
+    .populate('Category')
+    .exec((err, doc) => {
+      if(err){
+        res.status(500).json({
+          error: err
+        })
+      } else {
+        res.status(200).json({
+          message: 'sub category',
+          result: doc
+        })
+      }
+    })
 };
 
 export const addSubCategory = async (req, res) => {
@@ -18,13 +31,12 @@ export const addSubCategory = async (req, res) => {
     // console.log(req.body)
     const newSubcategory = new SubCategory(req.body);
     await newSubcategory.save((err) => {
-      if(err){
+      if (err) {
         res.status(500).json({ error: "server side error" });
       } else {
         res.status(200).json({ message: "sub category added successfully" });
       }
-    })
-
+    });
   } catch (err) {
     res.status(500).json({ error: "server side error" });
   }
